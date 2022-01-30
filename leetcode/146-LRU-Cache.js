e38e1a1d0afd254c72cda1c832c0bcd1c7d32e27
Dacheng Zhao <daczhao123@gmail.com>
2022-01-29 23:08:21 -0500

    LRU cache

--- leetcode/146-LRU-Cache.js
+++ leetcode/146-LRU-Cache.js
@@ -77,3 +77,100 @@ LRUCache.prototype.addNodeToDLL = function(node) {
     node.next = this.tail;
     this.tail.pre = node;
 }
+
+
+// javascript map is ordered
+class LRUCache {
+    constructor(capacity) {
+        this.map = new Map();
+        this.capacity = capacity;
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1;
+        const val = this.map.get(key);
+        this.map.delete(key);
+        this.map.set(key, val);
+        return val;
+    }
+
+    put(key, value) {
+        this.map.delete(key);
+        this.map.set(key, value);
+        if (this.map.size > this.capacity) {
+          const firstItem = this.map.keys().next().value;
+          this.map.delete(firstItem);
+        }
+    }
+}
+
+
+// using class
+/**
+ * @param {number} capacity
+ */
+ class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.map = {};
+        this.head = new DLLNode(null, null);
+        this.tail = new DLLNode(null, null);
+        this.head.next = this.tail;
+        this.tail.pre = this.head;
+    }
+    
+    get(key) {
+        if (!this.map[key]) {
+            return -1;
+        } else {
+            const currNode = this.map[key];
+            this.delete(currNode);
+            this.add(currNode);
+            
+            return currNode.value;
+        }
+    }
+    
+    put(key, value) {
+        const newNode = new DLLNode(key, value);
+        
+        if (this.map[key]) {
+            const oldNode = this.map[key];
+            this.map[key] = newNode;
+            this.add(newNode);
+            this.delete(oldNode);
+        } else {
+            this.add(newNode);
+            this.map[key] = newNode;
+            if (Object.keys(this.map).length > this.capacity) {
+                const lastNode = this.tail.pre;
+                this.delete(lastNode);
+                delete this.map[lastNode.key]
+            }
+        }
+    }
+    
+    add(node) {
+        const nextNode = this.head.next;
+        this.head.next = node;
+        node.pre = this.head;
+        node.next = nextNode;
+        nextNode.pre = node;
+    }
+    
+    delete(node) {
+        const preNode = node.pre;
+        const nextNode = node.next;
+        preNode.next = nextNode;
+        nextNode.pre = preNode;
+    }
+};
+
+class DLLNode {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.pre = null;
+        this.next = null;
+    }
+}
